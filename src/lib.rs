4ddda130371c83447c8a47dba64bbd2f12b9ca7c
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-03-22 22:57:59 +0100

    Make pulldown-cmark library rustc build system compliant

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,11 @@
 
 //! Pull parser for commonmark.
 
+// When compiled for the rustc compiler itself we want to make sure that this is
+// an unstable crate.
+#![cfg_attr(rustbuild, feature(staged_api))]
+#![cfg_attr(rustbuild, unstable(feature = "rustdoc", issue = "27812"))]
+
 pub mod html;
 
 #[macro_use]

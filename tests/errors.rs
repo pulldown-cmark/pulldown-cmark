3683e98fb7be29765b7827140ef37be7d32e0e99
mcarton <cartonmartin+git@gmail.com>
2017-08-02 22:37:42 +0200

    Fix parsing bugs in unterminated links

--- tests/errors.rs
+++ tests/errors.rs
@@ -5,7 +5,16 @@ fn test_wrong_code_block() {
     let markdown = r##"```
  * ```
  "##;
-    use pulldown_cmark::{Parser};
+    use pulldown_cmark::Parser;
 
     let _ = Parser::new(&markdown);
+}
+
+#[test]
+fn test_unterminated_link() {
+    let markdown = "[](\\";
+    use pulldown_cmark::Parser;
+
+    let parser = Parser::new(&markdown);
+    for _ in parser { }
 }

0a9e514d0d4c7b38c6965661a95723d2978e2893
Marcus Klaas <mail@marcusklaas.nl>
2019-03-08 14:45:43 +0100

    Add conditional compilation for SIMD code

--- benches/lib.rs
+++ benches/lib.rs
@@ -5,7 +5,7 @@ extern crate test;
 
 mod to_html {
     use pulldown_cmark::{Parser, Options, html};
-    use pulldown_cmark::escape::{escape_html_safe, escape_html_unsafe};
+    use pulldown_cmark::escape::{escape_html_secure, escape_html};
     use std::str::from_utf8;
 
     fn render_html(text: &str, opts: Options) -> String {
@@ -25,37 +25,37 @@ mod to_html {
     }
 
     #[bench]
-    fn escape_html_dense_unsafe(b: &mut test::Bencher) {
+    fn escape_html_dense_insecure(b: &mut test::Bencher) {
         let test_string = "<html><head><title>our amazing blog!</title><head><body>happy you are here. this is my
         very personal blog. hope u enjoy it. this is me <img alt=\"holiday in greece\" src=\"holiday.jph\"/></body>";
         let expected = "&lt;html&gt;&lt;head&gt;&lt;title&gt;our amazing blog!&lt;/title&gt;&lt;head&gt;&lt;body&gt;happy you are here. this is my
         very personal blog. hope u enjoy it. this is me &lt;img alt=&quot;holiday in greece&quot; src=&quot;holiday.jph&quot;/&gt;&lt;/body&gt;";
         let mut buf = String::with_capacity(1000);
 
-        b.iter(|| {
-            escape_html_unsafe(&mut buf, test_string);
+        b.iter(|| unsafe {
+            escape_html(buf.as_mut_vec(), test_string).unwrap();
             assert_eq!(expected, buf);
             buf.clear();
         });
     }
 
     #[bench]
-    fn escape_html_dense_safe(b: &mut test::Bencher) {
+    fn escape_html_dense_secure(b: &mut test::Bencher) {
         let test_string = "<html><head><title>our amazing blog!</title><head><body>happy you are here. this is my
         very personal blog. hope u enjoy it. this is me <img alt=\"holiday in greece\" src=\"holiday.jph\"/></body>";
         let expected = "&lt;html&gt;&lt;head&gt;&lt;title&gt;our amazing blog!&lt;/title&gt;&lt;head&gt;&lt;body&gt;happy you are here. this is my
         very personal blog. hope u enjoy it. this is me &lt;img alt=&quot;holiday in greece&quot; src=&quot;holiday.jph&quot;/&gt;&lt;/body&gt;";
         let mut buf = String::with_capacity(1000);
 
-        b.iter(|| {
-            escape_html_safe(&mut buf, test_string, false);
+        b.iter(|| unsafe {
+            escape_html_secure(buf.as_mut_vec(), test_string, false).unwrap();
             assert_eq!(expected, buf);
             buf.clear();
         });
     }
 
     #[bench]
-    fn escape_html_sparse(b: &mut test::Bencher) {
+    fn escape_html_sparse_insecure(b: &mut test::Bencher) {
         let test_string = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled
          and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain
          and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness
@@ -63,15 +63,15 @@ mod to_html {
          and easy to distinguish.";
         let mut buf = String::with_capacity(1000);
 
-        b.iter(|| {
-            escape_html_unsafe(&mut buf, test_string);
+        b.iter(|| unsafe {
+            escape_html(buf.as_mut_vec(), test_string).unwrap();
             assert_eq!(buf, test_string);
             buf.clear();
         });
     }
 
     #[bench]
-    fn escape_html_sparse_safe(b: &mut test::Bencher) {
+    fn escape_html_sparse_secure(b: &mut test::Bencher) {
         let test_string = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled
          and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain
          and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness
@@ -79,8 +79,8 @@ mod to_html {
          and easy to distinguish.";
         let mut buf = String::with_capacity(1000);
 
-        b.iter(|| {
-            escape_html_safe(&mut buf, test_string, false);
+        b.iter(|| unsafe {
+            escape_html_secure(buf.as_mut_vec(), test_string, false).unwrap();
             assert_eq!(buf, test_string);
             buf.clear();
         });
--- src/escape.rs
+++ src/escape.rs
@@ -106,50 +106,27 @@ static HTML_ESCAPES: [&'static str; 6] = [
         "&gt;"
 ];
 
-pub fn escape_html_unsafe(ob: &mut String, s: &str) {
+#[cfg(target_arch = "x86_64")]
+pub fn escape_html<W: Write>(mut w: W, s: &str) -> io::Result<()> {
     let bytes = s.as_bytes();
     let mut mark = 0;
 
-    scan_simd_insecure(bytes, 0, |i| {
+    scan_simd(bytes, 0, |i| {
         let replacement = HTML_ESCAPES[HTML_ESCAPE_TABLE[bytes[i] as usize] as usize];
-        ob.push_str(&s[mark..i]);
-        ob.push_str(replacement);
+        w.write_all(&bytes[mark..i])?;
+        w.write_all(replacement.as_bytes())?;
         mark = i + 1;  // all escaped characters are ASCII
-    });
-    ob.push_str(&s[mark..]);
+        Ok(())
+    })?;
+    w.write_all(&bytes[mark..])
 }
 
-// pub fn escape_html_safe(ob: &mut String, s: &str, secure: bool) {
-//     let bytes = s.as_bytes();
-//     let mut mark = 0;
-
-//     scan_simple(bytes, 0, |i| {
-//         let escape_ix = HTML_ESCAPE_TABLE[bytes[i] as usize] as usize;
-//         if escape_ix != 0 && (secure || escape_ix != 3) {
-//             ob.push_str(&s[mark..i]);
-//             ob.push_str(HTML_ESCAPES[escape_ix]);
-//             mark = i + 1;  // all escaped characters are ASCII
-//         }
-//     });
-
-//     ob.push_str(&s[mark..]);
-// }
-
-fn scan_simple<F: FnMut(usize) -> ()>(bytes: &[u8], mut i: usize, mut callback: F) {
-    let size = bytes.len();
-    while i < size {
-        match bytes[i..].iter().position(|&c| HTML_ESCAPE_TABLE[c as usize] != 0) {
-            Some(pos) => {
-                i += pos;
-            }
-            None => break,
-        }
-        callback(i);
-        i += 1;
-    }
+#[cfg(not(target_arch = "x86_64"))]
+pub fn escape_html<W: Write>(mut w: W, s: &str) -> io::Result<()> {
+    escape_html_secure(w, s, false)
 }
 
-pub fn escape_html_safe<W>(mut w: W, s: &str, secure: bool) -> io::Result<()>
+pub fn escape_html_secure<W>(mut w: W, s: &str, secure: bool) -> io::Result<()>
 where
     W: Write,
 {
@@ -176,12 +153,22 @@ where
         }
         i += 1;
     }
-    w.write_all(&bytes[mark..])?;
-    Ok(())
+    w.write_all(&bytes[mark..])
 }
 
-// does not scan b'/' and does not do  
-fn scan_simd_insecure<F: FnMut(usize) -> ()>(bytes: &[u8], mut offset: usize, mut callback: F) {
+/// Calls the given function with the index of every byte that is in
+/// [", &, <, >] and for no other byte.
+#[cfg(target_arch = "x86_64")]
+fn scan_simd<F>(bytes: &[u8], mut offset: usize, mut callback: F) -> io::Result<()>
+    where F: FnMut(usize) -> io::Result<()>
+{
+    if bytes.is_empty() {
+        // we need this special case for when we are passed an empty subslice
+        // that is exactly on a page boundary. we would try to read on a page
+        // that is potentially not ours and crash
+        return Ok(());
+    }
+
     let lower_vec = unsafe { _mm_set_epi8( 0, 62, 0, 60, 0, 0, 0, 0, 0, 38, 0, 0, 0, 34, 0, 127 ) };
     let upperbound = bytes.len().saturating_sub(16);
 
@@ -196,58 +183,55 @@ fn scan_simd_insecure<F: FnMut(usize) -> ()>(bytes: &[u8], mut offset: usize, mu
 
         while mask != 0 {
             let ix = mask.trailing_zeros();
-            callback(offset + ix as usize);
+            callback(offset + ix as usize)?;
             mask ^= mask & -mask;
         }
 
         offset += 16;
     }
 
-    // final iteration - read past buffer. but it should be fine *most of the time*
-    // because we're reading from a larger slice or something that's aligned to 16 bytes.
-    // hopefully. YOLO
-    let mut mask = unsafe {
-        let raw_ptr = transmute(bytes.as_ptr().offset(offset as isize));
-        let v = _mm_loadu_si128(raw_ptr);
-        let expected = _mm_shuffle_epi8(lower_vec, v);
-        let matches = _mm_cmpeq_epi8(expected, v);
-        _mm_movemask_epi8(matches)
-    };
+    // final iteration. there's two scenario's:
+    //  * the remaining bytes cross a 16-byte aligned chunk: in this case
+    //    we know that the next chunk is on a page assigned to our process
+    //    and it's safe to read (by the assertion that page sizes are multiples
+    //    of 16 bytes)
+    //  * the remaining bytes do not cross a 16-byte aligned chunk: reading
+    //    16 bytes more bytes could cross onto a new page which isn't ours
+    //    and crash the program. However, we since all the bytes are contained
+    //    in the current chunk, we can just read that and shift off the
+    //    bytes we aren't interested in
+    let alignment = (bytes.as_ptr() as usize) % 16;
+    let remaining_bytes = bytes.len() - offset;
 
-    // zero out the bytes we read past end of buffer
-    mask &= (1 << (bytes.len() - offset)) - 1;
-
-    while mask != 0 {
-        let ix = mask.trailing_zeros();
-        callback(offset + ix as usize);
-        mask ^= mask & -mask;
-    }
-}
-
-fn scan_simd<F: FnMut(usize) -> ()>(bytes: &[u8], mut offset: usize, mut callback: F) {
-    let lower_vec = unsafe { _mm_set_epi8( 47, 62, 0, 60, 0, 0, 0, 0, 0, 38, 0, 0, 0, 34, 0, 127 ) };
-    let upperbound = bytes.len().saturating_sub(15);
-    while offset < upperbound {
-        let mut mask = unsafe {
+    let mut mask = if alignment + remaining_bytes > 16 {
+        unsafe {
             let raw_ptr = transmute(bytes.as_ptr().offset(offset as isize));
             let v = _mm_loadu_si128(raw_ptr);
             let expected = _mm_shuffle_epi8(lower_vec, v);
             let matches = _mm_cmpeq_epi8(expected, v);
             _mm_movemask_epi8(matches)
-        };
-
-        while mask != 0 {
-            let ix = mask.trailing_zeros();
-            callback(offset + ix as usize);
-            mask ^= mask & -mask;
         }
+    } else {
+        unsafe {
+            let raw_ptr = transmute(bytes.as_ptr().offset(offset as isize - alignment as isize));
+            // yay, aligned load!
+            let v = _mm_load_si128(raw_ptr);
+            let expected = _mm_shuffle_epi8(lower_vec, v);
+            let matches = _mm_cmpeq_epi8(expected, v);
+            // shift off the bytes we aren't interested in
+            _mm_movemask_epi8(matches) >> alignment
+        }
+    };
 
-        offset += 16;
+    // zero out the bytes we read past end of buffer
+    mask &= (1 << remaining_bytes) - 1;
+
+    while mask != 0 {
+        let ix = mask.trailing_zeros();
+        callback(offset + ix as usize)?;
+        mask ^= mask & -mask;
     }
-
-    // we can only do simd if we can atleast 16 bytes at once
-    // do final bytes old fashioned way
-    scan_simple(&bytes, offset, callback);        
+    Ok(())
 }
 
 #[cfg(test)]
@@ -257,32 +241,25 @@ mod html_scan_tests {
     #[test]
     fn scan_simd_works() {
         let mut vec = Vec::new();
-        scan_simd("&aXa/aa\"".as_bytes(), 0, |ix| vec.push(ix));
-        assert_eq!(vec, vec![0, 4, 7]);
+        scan_simd("&aXaaa\"".as_bytes(), 0, |ix| { vec.push(ix); Ok(()) }).unwrap();
+        assert_eq!(vec, vec![0, 6]);
     }
     
     #[test]
     fn combined_scan_works() {
         let mut vec = Vec::new();
-        scan_simd("&aXa/aa.a'aa9a<>aab&".as_bytes(), 0, |ix| vec.push(ix));
-        assert_eq!(vec, vec![0, 4, 14, 15, 19]);
-    }
-    
-    #[test]
-    fn scan_simpl_works() {
-        let mut vec = Vec::new();
-        scan_simple("&aXa/aa.a'aa9a<>".as_bytes(), 0, |ix| vec.push(ix));
-        assert_eq!(vec, vec![0, 4, 14, 15]);
+        scan_simd("&aXaaaa.a'aa9a<>aab&".as_bytes(), 0, |ix| { vec.push(ix); Ok(()) }).unwrap();
+        assert_eq!(vec, vec![0, 14, 15, 19]);
     }
 
     // only match these bytes, and when we match them, match them 16 times
     #[test]
     fn only_right_bytes_matched() {
         for b in 0..255u8 {
-            let right_byte = b == b'&' || b == b'/' || b == b'<' || b == b'>' || b == b'"';
+            let right_byte = b == b'&' || b == b'<' || b == b'>' || b == b'"';
             let vek = vec![b; 16];
             let mut match_count = 0;
-            scan_simd(&vek, 0, |_| { match_count += 1; });
+            scan_simd(&vek, 0, |_| { match_count += 1; Ok(()) }).unwrap();
             assert!((match_count > 0) == (match_count == 16));
             assert_eq!((match_count == 16), right_byte, "match_count: {}, byte: {:?}", match_count, b as char);
         }

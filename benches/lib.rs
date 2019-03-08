07d200d16057b0314401ac538776cc3b6ee7d4aa
Marcus Klaas <mail@marcusklaas.nl>
2019-03-08 14:49:40 +0100

    Minor cleanup

--- benches/lib.rs
+++ benches/lib.rs
@@ -24,64 +24,57 @@ mod to_html {
         b.iter(|| render_html(&input, opts));
     }
 
+    static DENSE_TEST_STRING: &str = "<html><head><title>our amazing blog!</title><head><body>happy you are here. this is my
+        very personal blog. hope u enjoy it. this is me <img alt=\"holiday in greece\" src=\"holiday.jph\"/></body>";
+    static DENSE_EXPECTED: &str = "&lt;html&gt;&lt;head&gt;&lt;title&gt;our amazing blog!&lt;/title&gt;&lt;head&gt;&lt;body&gt;happy you are here. this is my
+        very personal blog. hope u enjoy it. this is me &lt;img alt=&quot;holiday in greece&quot; src=&quot;holiday.jph&quot;/&gt;&lt;/body&gt;";
+
     #[bench]
     fn escape_html_dense_insecure(b: &mut test::Bencher) {
-        let test_string = "<html><head><title>our amazing blog!</title><head><body>happy you are here. this is my
-        very personal blog. hope u enjoy it. this is me <img alt=\"holiday in greece\" src=\"holiday.jph\"/></body>";
-        let expected = "&lt;html&gt;&lt;head&gt;&lt;title&gt;our amazing blog!&lt;/title&gt;&lt;head&gt;&lt;body&gt;happy you are here. this is my
-        very personal blog. hope u enjoy it. this is me &lt;img alt=&quot;holiday in greece&quot; src=&quot;holiday.jph&quot;/&gt;&lt;/body&gt;";
         let mut buf = String::with_capacity(1000);
 
         b.iter(|| unsafe {
-            escape_html(buf.as_mut_vec(), test_string).unwrap();
-            assert_eq!(expected, buf);
+            escape_html(buf.as_mut_vec(), DENSE_TEST_STRING).unwrap();
+            assert_eq!(DENSE_EXPECTED, buf);
             buf.clear();
         });
     }
 
     #[bench]
     fn escape_html_dense_secure(b: &mut test::Bencher) {
-        let test_string = "<html><head><title>our amazing blog!</title><head><body>happy you are here. this is my
-        very personal blog. hope u enjoy it. this is me <img alt=\"holiday in greece\" src=\"holiday.jph\"/></body>";
-        let expected = "&lt;html&gt;&lt;head&gt;&lt;title&gt;our amazing blog!&lt;/title&gt;&lt;head&gt;&lt;body&gt;happy you are here. this is my
-        very personal blog. hope u enjoy it. this is me &lt;img alt=&quot;holiday in greece&quot; src=&quot;holiday.jph&quot;/&gt;&lt;/body&gt;";
         let mut buf = String::with_capacity(1000);
 
         b.iter(|| unsafe {
-            escape_html_secure(buf.as_mut_vec(), test_string, false).unwrap();
-            assert_eq!(expected, buf);
+            escape_html_secure(buf.as_mut_vec(), DENSE_TEST_STRING, false).unwrap();
+            assert_eq!(DENSE_EXPECTED, buf);
             buf.clear();
         });
     }
 
-    #[bench]
-    fn escape_html_sparse_insecure(b: &mut test::Bencher) {
-        let test_string = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled
+    static SPARSE_TEST_STRING: &str = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled
          and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain
          and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness
          of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple
          and easy to distinguish.";
+
+    #[bench]
+    fn escape_html_sparse_insecure(b: &mut test::Bencher) {
         let mut buf = String::with_capacity(1000);
 
         b.iter(|| unsafe {
-            escape_html(buf.as_mut_vec(), test_string).unwrap();
-            assert_eq!(buf, test_string);
+            escape_html(buf.as_mut_vec(), SPARSE_TEST_STRING).unwrap();
+            assert_eq!(buf, SPARSE_TEST_STRING);
             buf.clear();
         });
     }
 
     #[bench]
     fn escape_html_sparse_secure(b: &mut test::Bencher) {
-        let test_string = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled
-         and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain
-         and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness
-         of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple
-         and easy to distinguish.";
         let mut buf = String::with_capacity(1000);
 
         b.iter(|| unsafe {
-            escape_html_secure(buf.as_mut_vec(), test_string, false).unwrap();
-            assert_eq!(buf, test_string);
+            escape_html_secure(buf.as_mut_vec(), SPARSE_TEST_STRING, false).unwrap();
+            assert_eq!(buf, SPARSE_TEST_STRING);
             buf.clear();
         });
     }

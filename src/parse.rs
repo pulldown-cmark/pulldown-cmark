32df27a46038e7ce6fab1642068cea777f771c14
Joshua Nelson <jyn514@gmail.com>
2020-05-31 09:49:33 -0400

    Add broken_links_called_only_once test

--- src/parse.rs
+++ src/parse.rs
@@ -3118,4 +3118,19 @@ mod test {
         }
         assert_eq!(found, 1);
     }
+
+    #[test]
+    fn broken_links_called_only_once() {
+        use std::cell::Cell;
+
+        let markdown = "See also [`g()`][crate::g].";
+        let times_called = Cell::new(0);
+        let callback = |_: &str, _: &str| {
+            times_called.set(times_called.get() + 1);
+            None
+        };
+        let parser = Parser::new_with_broken_link_callback(markdown, Options::empty(), Some(&callback));
+        for _ in parser {}
+        assert_eq!(times_called.get(), 1);
+    }
 }

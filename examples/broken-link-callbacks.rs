fe431636409624c9df99a80658ed30114c532ae7
Marcus Klaas de Vries <mail@marcusklaas.nl>
2020-08-31 12:10:13 +0200

    Clean up new callback design a bit

--- examples/broken-link-callbacks.rs
+++ examples/broken-link-callbacks.rs
@@ -9,12 +9,12 @@ fn main() {
     // Setup callback that sets the URL and title when it encounters
     // a reference to our home page.
     let callback = &mut |broken_link: BrokenLink| {
-        if broken_link.reference.as_ref() == "my website" {
+        if broken_link.reference == "my website" {
             println!(
                 "Replacing the markdown `{}` of type {:?} with a working link",
                 &input[broken_link.span], broken_link.link_type,
             );
-            Some(("http;//example.com".into(), "my example website".into()))
+            Some(("http://example.com".into(), "my example website".into()))
         } else {
             None
         }
@@ -29,7 +29,7 @@ fn main() {
 
     // Check that the output is what we expected.
     let expected_html: &str =
-        "<p>Hello world, check out <a href=\"http;//example.com\" title=\"my example website\">my website</a>.</p>\n";
+        "<p>Hello world, check out <a href=\"http://example.com\" title=\"my example website\">my website</a>.</p>\n";
     assert_eq!(expected_html, &html_output);
 
     // Write result to stdout.

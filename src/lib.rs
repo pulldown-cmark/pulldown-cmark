bae9348244f9109025a6e5eff010e1a2cb6363a7
Bruce Mitchener <bruce.mitchener@gmail.com>
2023-07-18 10:50:19 +0700

    chore: Update bitflags from 1.3.x to 2.x

--- src/lib.rs
+++ src/lib.rs
@@ -358,6 +358,7 @@ pub enum Alignment {
 bitflags::bitflags! {
     /// Option struct containing flags for enabling extra features
     /// that are not part of the CommonMark spec.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub struct Options: u32 {
         const ENABLE_TABLES = 1 << 1;
         /// GitHub-compatible footnote syntax.
